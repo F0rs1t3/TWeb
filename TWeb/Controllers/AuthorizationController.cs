d7691ba1cb9ee3a7587aff3b69c6f25e5e4dd809
F0rs1t <vitcal.calae@gmail.com>
2025-05-14 23:21:25 +0300

    Updated design and interface ( html/css )

---
+++ TWeb/Controllers/AuthorizationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TWeb.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
