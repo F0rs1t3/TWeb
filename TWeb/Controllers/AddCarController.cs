36dfb41d972677fac53ed3aa100c3c068e9bf31e
F0rs1t <vitcal.calae@gmail.com>
2025-05-19 12:44:57 +0300

    Added more pages and modified design

---
+++ TWeb/Controllers/AddCarController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TWeb.Controllers
+{
+    public class AddCarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCar(string Make, string Model, int Year, decimal Price, string Description)
+        {
+            // You can save the data to the database here
+            // For now, just redirect or return a confirmation view
+
+            TempData["Message"] = $"Car {Make} {Model} added successfully!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TWeb/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TWeb.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
